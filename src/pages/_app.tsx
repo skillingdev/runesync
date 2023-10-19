290db942bdd532ac634815e42b86fe469edd1ba1
skillingdev <skilling.dev@gmail.com>
2023-10-19 09:44:59 -0400

    Try tailwindcss & nextui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app'
+import { NextUIProvider } from "@nextui-org/react";
+
+import '~/styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <NextUIProvider>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@nextui-org/button";
+
 export default function Home() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Press me to get started!</Button>
+    </div>
   )
 }
